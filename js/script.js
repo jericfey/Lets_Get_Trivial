911fa72704f3b8cde13421943a5e29004fec6d81
Jules <juliannewood2413@gmail.com>
2020-10-29 21:15:11 -0600

    updated pseudocode for js file

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+//VARIABLES 
+  //will need variables for:
+   //userName
+   //array of random quiz questions
+   //variable for requestApi
+   //variable for user score
+   //
+
+
+
+
+
+//API CALL
+  //AJAX FUNCTION
+
+
+
+//FUNCTIONS
+    //will need functions for quiz pages,
+    //function for score counter and iterating through random questions array
+    //conditionals for number of incorrect answers
+        //for wrong answers - if 3 incorrect user gets a whammy
+        //for correct answers - user accumulates 10 points
+
+
+
+
+
+//EVENT LISTENERS
+    //will need event listeners for:
+     //submit that enters user name into storage,
+     //click listener for each category button and links user to specific category page
+
+
+
+
+
+
+
+
+
+})
