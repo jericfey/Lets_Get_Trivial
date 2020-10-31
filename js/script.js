da3a1495f4a95bec9dcfe9a17d441b8ab5911d8b
Jeri Fey <jericfey@gmail.com>
2020-10-31 10:07:31 -0600

    Create answers array from incorrect and correct answers

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   //FUNCTIONS
 
+  //Clear the div before displaying the next question/answer set
+  $("#questions").empty();
+
   //Pull Art Trivia questions and answers
   var getArtTrivia = function () {
     var requestURL =
@@ -10,7 +13,125 @@ $(document).ready(function () {
       url: requestURL,
       method: "GET",
     }).then(function (data) {
-      console.log(data);
+      // console.log(
+      //   "Art Data: ",
+      //   data
+      // );
+      //VARIABLE DECLARATIONS
+      //dynamically create div where the HTML will be placed
+      var trivia = $("#questions");
+      var question = $("<h3>");
+      var answersEl = $("<h4>");
+      var answers = [];
+
+      //get the data into the element that was created above
+      question.text(data.results[0].question);
+      // console.log(data.results[0].question);
+      answersEl.text(data.results[0].correct_answer);
+
+      //build array for answers including both incorrect and correct answers
+
+      var correct_answer = data.results[0].correct_answer;
+      var incorrect_answers = data.results[0].incorrect_answers;
+      answers.push(correct_answer);
+      answers.push(incorrect_answers);
+      // console.log("Answers Data: ", answers);
+
+      // console.log("Correct Answer Data: ", data.results[0].correct_answer);
+      // console.log(
+      //   "Incorrect Answers Data: ",
+      //   data.results[0].incorrect_answers
+      // );
+
+      //append elements
+      trivia.append(question);
+      trivia.append(answersEl);
+      trivia.append(answers);
+    });
+  };
+
+  //Pull Music Trivia questions and answers
+  var getMusicTrivia = function () {
+    var requestURL =
+      "https://opentdb.com/api.php?amount=15&category=12&type=multiple";
+
+    $.ajax({
+      url: requestURL,
+      method: "GET",
+    }).then(function (data) {
+      console.log("Music Data: ", data);
+      //VARIABLE DECLARATIONS
+      //dynamically create div where the HTML will be placed
+      var trivia = $("#questions");
+      var question = $("<h3>");
+
+      //get the data into the element that was created above
+      question.text(data.results[0].question);
+      // console.log(data.results[0].question);
+
+      //append elements
+      trivia.append(question);
+    });
+  };
+
+  //Pull Sports Trivia questions and answers
+  var getSportsTrivia = function () {
+    var requestURL =
+      "https://opentdb.com/api.php?amount=15&category=21&type=multiple";
+
+    $.ajax({
+      url: requestURL,
+      method: "GET",
+    }).then(function (data) {
+      console.log("Sports Data: ", data);
+      //VARIABLE DECLARATIONS
+      //dynamically create div where the HTML will be placed
+      var trivia = $("#questions");
+      var question = $("<h3>");
+
+      //get the data into the element that was created above
+      question.text(data.results[0].question);
+      // console.log(data.results[0].question);
+
+      //append elements
+      trivia.append(question);
+    });
+  };
+
+  //Pull Books Trivia questions and answers
+  var getBooksTrivia = function () {
+    var requestURL =
+      "https://opentdb.com/api.php?amount=15&category=10&type=multiple";
+
+    $.ajax({
+      url: requestURL,
+      method: "GET",
+    }).then(function (data) {
+      console.log("Books Data: ", data);
+      //VARIABLE DECLARATIONS
+      //dynamically create div where the HTML will be placed
+      var trivia = $("#questions");
+      var question = $("<h3>");
+
+      //get the data into the element that was created above
+      question.text(data.results[0].question);
+      // console.log(data.results[0].question);
+
+      //append elements
+      trivia.append(question);
+    });
+  };
+
+  //Pull History Trivia questions and answers
+  var getHistoryTrivia = function () {
+    var requestURL =
+      "https://opentdb.com/api.php?amount=15&category=23&type=multiple";
+
+    $.ajax({
+      url: requestURL,
+      method: "GET",
+    }).then(function (data) {
+      console.log("History Data: ", data);
       //VARIABLE DECLARATIONS
       //dynamically create div where the HTML will be placed
       var trivia = $("#questions");
@@ -27,11 +148,15 @@ $(document).ready(function () {
 
   //EVENT LISTENERS
   $("#art").on("click", getArtTrivia);
-  // $("music").on("click", getMusicTrivia);
-  // $("sports").on("click", getSportsTrivia);
-  // $("books").on("click", getBooksTrivia);
-  // $("history").on("click", getHistoryTrivia);
+  $("#music").on("click", getMusicTrivia);
+  $("#sports").on("click", getSportsTrivia);
+  $("#books").on("click", getBooksTrivia);
+  $("#history").on("click", getHistoryTrivia);
 });
 
+//I could not get the event listener to work with the button class of "art", added id="art" to button
 //Added JavaScript and jQuery links before body element end
 //Added div class="questions"
+//How to format returned question like this "On which Beatles album would you find the song &#039;Eleanor Rigby&#039;?"
+//.empty() isn't working to clear the div before going to the next question
+//Will need to randomize correct_answer with incorrect_answers by appending the array for answers with incorrect_answers and correct_answer
