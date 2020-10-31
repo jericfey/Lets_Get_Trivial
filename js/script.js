e4f9c8b256ac666de6151e2f1db6c3a706c7a90c
Jeri Fey <jericfey@gmail.com>
2020-10-31 08:35:56 -0600

    Addes js that pulls art trivia question into div

--- js/script.js
+++ js/script.js
@@ -11,13 +11,27 @@ $(document).ready(function () {
       method: "GET",
     }).then(function (data) {
       console.log(data);
+      //VARIABLE DECLARATIONS
+      //dynamically create div where the HTML will be placed
+      var trivia = $("#questions");
+      var question = $("<h3>");
+
+      //get the data into the element that was created above
+      question.text(data.results[0].question);
+      // console.log(data.results[0].question);
+
+      //append elements
+      trivia.append(question);
     });
   };
 
   //EVENT LISTENERS
-  $("art").on("click", getArtTrivia);
+  $("#art").on("click", getArtTrivia);
   // $("music").on("click", getMusicTrivia);
   // $("sports").on("click", getSportsTrivia);
   // $("books").on("click", getBooksTrivia);
   // $("history").on("click", getHistoryTrivia);
 });
+
+//Added JavaScript and jQuery links before body element end
+//Added div class="questions"
