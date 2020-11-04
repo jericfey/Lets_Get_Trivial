370d7b4b28dfcb65b8899a0e1e23e1b47f2ada21
KcRaymond <kaseyleigh1978@gmail.com>
2020-11-03 20:23:24 -0700

    final file upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$("#art").click(function(){
+    window.location.href='./art.html';
+})
+$("#books").click(function(){
+    window.location.href='./books.html';
+})
+$("#history").click(function(){
+    window.location.href='./history.html';
+})
+$("#music").click(function(){
+    window.location.href='./music.html';
+})
+$("#sports").click(function(){
+    window.location.href='./sports.html';
+})
+$("#home").click(function() {
+    window.location.href='./index.html'
+})
+$("#home-category").click(function() {
+    window.location.href='./index.html'
+})
