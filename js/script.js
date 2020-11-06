ab1cd7fcbac44e078473a161d7b6e4af8965004d
Jeri Fey <jericfey@gmail.com>
2020-11-05 18:37:38 -0700

    Modified answer buttons to correct random selection

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
   //FUNCTIONS
   let questionsEl = document.getElementById("questions");
-
+  var correctAnswer = "";
   //Pull Trivia questions and answers based on category selected by player
   function getTrivia(category) {
     //Clear the div before displaying the next question/answer set
     $("#questions").empty();
-
     let requestURL =
       "https://opentdb.com/api.php?amount=15&category=" +
       category +
@@ -17,57 +16,47 @@ $(document).ready(function () {
       method: "GET",
     }).then(function (data) {
       console.log("Data: ", data);
-
       //VARIABLE DECLARATIONS
       //dynamically create div where the HTML will be placed
       let trivia = $("#questions");
       let question = $("<h3>");
       // let answersEl = $("<h3>");
       let answers = [];
-
       //get the data into the element that was created above
-      question.html(data.results[4].question);
-      console.log("Question:", data.results[4].question);
+      let randomIndex = Math.floor(Math.random() * 15);
+      console.log(randomIndex);
+      question.html(data.results[randomIndex].question);
+      console.log("Question:", data.results[randomIndex].question);
       trivia.append(question);
-
       //build array for answers including both incorrect and correct answers
-      let correctAnswer = data.results[3].correct_answer;
-      let incorrectAnswers = data.results[3].incorrect_answers;
+      correctAnswer = data.results[randomIndex].correct_answer; //using the number of the index didn't match
+      let incorrectAnswers = data.results[randomIndex].incorrect_answers;
       // answers.push(correctAnswer);
       // answers = answers.concat(incorrectAnswers);
       answers = [correctAnswer, ...incorrectAnswers];
-
       //loop through answers
-
-      for (
-        let i = 0, answersIndex = 4;
-        i < answers.length;
-        i++, answersIndex--
-      ) {
-        let randomIndex = Math.floor(Math.random() * answersIndex);
+      for (let i = 0; i < 4; i++) {
+        let randomPosition = Math.floor(Math.random() * answers.length);
         let answersBtn = document.createElement("button");
-
         answersBtn.setAttribute("class", "w3-btn");
-
-        answersBtn.textContent = answers[randomIndex];
-
+        answersBtn.setAttribute("id", i);
+        answersBtn.textContent = answers[randomPosition]; //added a random position variable so the answers append to random postitions,
+        // then spliced that position by one, so the function is forced to put answer elsewhere
         // Append the answersBtn somewhere
         questionsEl.appendChild(answersBtn);
+        answers.splice(randomPosition, 1);
+        // console.log(answers)
       }
-
       // answersEl.html(answers);
-      console.log("Answers:", answers);
-
+      // console.log("Answers:", answers);
       //Appends array of answers to the page
       // trivia.append(answersEl);
       // trivia.append(answers);
     });
   }
-
   //Pull random Chuck Norris Joke
   let getChuck = function () {
     let requestURL = "https://api.chucknorris.io/jokes/random";
-
     $.ajax({
       url: requestURL,
       method: "GET",
@@ -81,8 +70,8 @@ $(document).ready(function () {
   };
   //EVENT LISTENERS
   // $(".w3-btn").on("click", getTrivia);
+  var category;
   $(".w3-btn").on("click", function () {
-    let category;
     switch (this.id) {
       case "art":
         category = 25;
@@ -102,23 +91,51 @@ $(document).ready(function () {
     }
     getTrivia(category);
   });
-
   // $("#music").on("click", getTrivia);
   // $("#sports").on("click", getTrivia);
   // $("#books").on("click", getTrivia);
   // $("#history").on("click", getTrivia);
   // $("#chuck").on("click", getChuck);
-
+  //CLICK EVENTS
   $("#home").click(function () {
-    window.location.href = "./index.html";
+    window.location.href = "./index.html"; // CHANGE TO CURRENT PAGES
   });
   $("#home-category").click(function () {
     window.location.href = "./index.html";
   });
+  $("#play").click(function () {
+    window.location.href = "./game.html";
+  });
+  $("#playAgain").click(function () {
+    window.location.href = "./index.html";
+  });
+  //FUNCTION AND VARIABLES TO KEEP TRACK OF HIGH SCORE AND RANDOMIZE QUESTIONS/ANSWERS
+  var historicalCorrect = 0; // user score
+  var qCounter = 0; // to keep track of what question the user is on
+  if (localStorage.getItem("historicalCorrect")) {
+    historicalCorrect = parseInt(localStorage.getItem("historicalCorrect"));
+  }
+  $("#questions").on("click", "button", function () {
+    console.log(this.textContent);
+    if (this.textContent == correctAnswer) {
+      // console.log("Yo that's right bro")
+      historicalCorrect += 10;
+      qCounter++;
+      console.log(qCounter);
+      localStorage.setItem("historicalCorrect", historicalCorrect);
+    } else {
+      console.log("Nah"); //WHAMMY function/gif goes here
+    }
+    getTrivia(category);
+  });
+  //LOCAL STORAGE
+  var userName = document.getElementById("username");
+  userName.value = localStorage.getItem("lastUsername") || "";
+  console.log(userName);
+  document
+    .getElementById("highScoreForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      localStorage.setItem("lastUsername", userName.value);
+    });
 });
-
-//Added JavaScript and jQuery links before body element end
-//Added div class="questions"
-//How to format returned question like this "On which Beatles album would you find the song &#039;Eleanor Rigby&#039;?"
-//.empty() isn't working to clear the div before going to the next question
-//Will need to randomize correct_answer with incorrect_answers by appending the array for answers with incorrect_answers and correct_answer
