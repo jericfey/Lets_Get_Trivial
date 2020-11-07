994e701d06b67ad90df782075209651dbb527037
Jeri Fey <jericfey@gmail.com>
2020-11-06 21:22:25 -0700

    added whammy gifs

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
   //FUNCTIONS
   let questionsEl = document.getElementById("questions");
   var correctAnswer = "";
+  var randomImage = [
+    "fortuneteller-whammy.gif",
+    "sleepy-wham.gif",
+    "sports-whammy.gif",
+    "bon-voyage-whammy.gif",
+  ];
 
   $("#whammies").hide();
   //Pull Trivia questions and answers based on category selected by player
@@ -12,29 +18,26 @@ $(document).ready(function () {
       "https://opentdb.com/api.php?amount=15&category=" +
       category +
       "&type=multiple";
-    console.log("URL:", requestURL);
+    // console.log("URL:", requestURL);
     $.ajax({
       url: requestURL,
       method: "GET",
     }).then(function (data) {
-      console.log("Data: ", data);
+      // console.log("Data: ", data);
       //VARIABLE DECLARATIONS
       //dynamically create div where the HTML will be placed
       let trivia = $("#questions");
       let question = $("<h3>");
-      // let answersEl = $("<h3>");
       let answers = [];
-      //get the data into the element that was created above
       let randomIndex = Math.floor(Math.random() * 15);
-      console.log(randomIndex);
+      // console.log(randomIndex);
       question.html(data.results[randomIndex].question);
-      console.log("Question:", data.results[randomIndex].question);
+      // console.log("Question:", data.results[randomIndex].question);
       trivia.append(question);
-      //build array for answers including both incorrect and correct answers
       correctAnswer = data.results[randomIndex].correct_answer; //using the number of the index didn't match
       let incorrectAnswers = data.results[randomIndex].incorrect_answers;
-      // answers.push(correctAnswer);
-      // answers = answers.concat(incorrectAnswers);
+
+      //build array for answers including both incorrect and correct answers
       answers = [correctAnswer, ...incorrectAnswers];
       //loop through answers
       for (let i = 0; i < 4; i++) {
@@ -42,18 +45,17 @@ $(document).ready(function () {
         let answersBtn = document.createElement("button");
         answersBtn.setAttribute("class", "w3-btn");
         answersBtn.setAttribute("id", i);
-        answersBtn.textContent = answers[randomPosition]; //added a random position variable so the answers append to random postitions,
+        answersBtn.textContent = answers[randomPosition];
+        // answersBtn.html(answers[randomPosition]);
+        console.log("AnswersBtn:", answersBtn);
+        //added a random position variable so the answers append to random postitions,
         // then spliced that position by one, so the function is forced to put answer elsewhere
         // Append the answersBtn somewhere
         questionsEl.appendChild(answersBtn);
         answers.splice(randomPosition, 1);
         // console.log(answers)
       }
-      // answersEl.html(answers);
-      // console.log("Answers:", answers);
-      //Appends array of answers to the page
-      // trivia.append(answersEl);
-      // trivia.append(answers);
+      
     });
   }
   //Pull random Chuck Norris Joke
@@ -63,7 +65,7 @@ $(document).ready(function () {
       url: requestURL,
       method: "GET",
     }).then(function (data) {
-      console.log(data.value);
+      // console.log(data.value);
       let trivia = $("#jokes");
       let joke = $("<h3>");
       joke.html(data.value);
@@ -93,11 +95,7 @@ $(document).ready(function () {
     }
     getTrivia(category);
   });
-  // $("#music").on("click", getTrivia);
-  // $("#sports").on("click", getTrivia);
-  // $("#books").on("click", getTrivia);
-  // $("#history").on("click", getTrivia);
-  // $("#chuck").on("click", getChuck);
+
   //CLICK EVENTS
   $("#home").click(function () {
     window.location.href = "./index.html"; // CHANGE TO CURRENT PAGES
@@ -114,36 +112,45 @@ $(document).ready(function () {
   //FUNCTION AND VARIABLES TO KEEP TRACK OF HIGH SCORE AND RANDOMIZE QUESTIONS/ANSWERS
   var historicalCorrect = 0; // user score
   var qCounter = 0; // to keep track of what question the user is on
-  var wrongCount = 0;
+  var wrongCount = 0; //count of wrong answers
   // for (let i = 0; i < 10; i++) {
   if (localStorage.getItem("historicalCorrect")) {
     historicalCorrect = parseInt(localStorage.getItem("historicalCorrect"));
   }
+  if (localStorage.getItem("wrongCount")) {
+    wrongCount = parseInt(localStorage.getItem("wrongCount"));
+  }
   $("#questions").on("click", "button", function () {
-    console.log(this.textContent);
-    if (qCounter < 2 || wrongCount < 4) {
+    console.log("Answer selected", this.textContent);
+    if (qCounter < 2 || wrongCount < 3) {
       if (this.textContent == correctAnswer) {
         // console.log("Yo that's right bro")
         historicalCorrect += 10;
         qCounter++;
         // add set timeout button
-        console.log(qCounter);
+        console.log("qCounter", qCounter);
         localStorage.setItem("historicalCorrect", historicalCorrect);
       } else {
         wrongCount++;
+        localStorage.setItem("wrongCount", wrongCount);
         $("#questions").hide(3000, function () {
-          console.log("Hide Questions");
+          // console.log("Hide Questions");
           $("#whammies").show(5000, function () {
-            console.log("show whammies");
+            // console.log("show whammies");
             $("#whammies").hide();
             $("#questions").show();
+            
           });
         });
 
-        console.log("Nah"); //WHAMMY function/gif goes here
+        console.log("Wrong answer selected");
+        //WHAMMY function/gif goes here
       }
       getTrivia(category);
     } else {
+      //clear local storage at end of game
+      localStorage.clear();
+      //send player to the final page
       window.location.href = "./highscore_win.html";
     }
   });
@@ -163,21 +170,12 @@ $(document).ready(function () {
       winnerName.append(lastUsername);
     });
 });
-//Added JavaScript and jQuery links before body element end
-//Added div class="questions"
+
 //How to format returned question like this "On which Beatles album would you find the song &#039;Eleanor Rigby&#039;?"
-//.empty() isn't working to clear the div before going to the next question
-//Will need to randomize correct_answer with incorrect_answers by appending the array for answers with incorrect_answers and correct_answer
-//     $(".gif").hover(
-//         function()
-//         {
-//           var src = $(this).attr("src");
-//           $(this).attr("src", src.replace(/\.png$/i, ".gif"));
-//         },
-//         function()
-//         {
-//           var src = $(this).attr("src");
-//           $(this).attr("src", src.replace(/\.gif$/i, ".png"));
-//         });
-//see if we can have the highscore page js in the saem script file, otherwise create new one. link button
-//to highscore page
+
+//add wrongCount to local storage when a wrong answer is selected
+//Clear local storage at the end of the game
+//play sound on wrong answer
+//play sound on correct answer
+//hide category buttons when the game is being played
+//Need start over button when buttons are hidden during game play
